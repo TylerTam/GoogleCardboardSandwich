26ffd14c066f9d3e901fd1e2624ed69b53bc54cd
TylerTam <46612488+TylerTam@users.noreply.github.com>
2020-03-11 11:08:37 -0400

    Added animations to cutting board and stove

---
+++ Assets/_SandwichMaker/Scripts/KitchenTools/KitchenTools_VisualController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitchenTools_VisualController : MonoBehaviour
+{
+    public Animator m_aCont;
+
+    public string m_animBool = "Using";
+
+    public void StartAnimation()
+    {
+        m_aCont.SetBool(m_animBool, true);
+    }
+    public void StopAnimation()
+    {
+        m_aCont.SetBool(m_animBool, false);
+    }
+}
